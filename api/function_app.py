e9c1e0c17e080a93c3ca09336870f6a9a76df1f7
lordrelv <m.serebour.21@unimail.winchester.ac.uk>
2024-05-22 15:48:50 +0100

    22.05.2024 1548

--- api/function_app.py
+++ api/function_app.py
@@ -17,6 +17,25 @@ def create_dog(req: functions.HttpRequest, documentForDog: functions.Out[functio
         return functions.HttpResponse(status_code = 204)
     else: 
         return functions.HttpResponse(status_code = 400)
+    
+
+
+
+
+
+@application.function_name(name = "createWalker")
+@application.route(route = "createWalker", auth_level = functions.AuthLevel.ANONYMOUS)
+@application.cosmos_db_output(arg_name = "documentForWalker", database_name = "waqqly_web_app_database", container_name = "waqqly_web_app_container", connection = "CosmosDbConnectionSetting")
+def create_walker(req: functions.HttpRequest, documentForWalker: functions.Out[functions.Document]) -> functions.HttpResponse:
+    id_for_walker = ''.join(random.sample(string.ascii_letters+string.digits, 10))
+    walkers_name = req.params.get('walkersname')
+    walkers_location = req.params.get('walkerslocation')
+    walkers_phone_number = req.params.get('walkersphonenumber')
+    if walkers_name and walkers_location and walkers_phone_number:
+        documentForWalker.set(functions.Document.from_dict({"id": id_for_walker, "walkers_name": walkers_name, "walkers_location": walkers_location, "walkers_phone_number": walkers_phone_number}))
+        return functions.HttpResponse(status_code = 204)
+    else: 
+        return functions.HttpResponse(status_code = 400)
 
 
     
